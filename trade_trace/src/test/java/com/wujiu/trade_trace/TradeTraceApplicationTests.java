f5228ba62ece9dc7e9ee964a0acbe53ff87a48ac
fanhao.mag <fanhao.mag@bytedance.com>
2024-12-02 11:47:00 +0800

    init project

---
+++ trade_trace/src/test/java/com/wujiu/trade_trace/TradeTraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wujiu.trade_trace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeTraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
